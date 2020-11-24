101ff2f6e6f5f8804e589b054db976c1c851094c
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-11-24 13:37:33 +0800

    Create updatebin.sh

---
+++ updatebin.sh
@@ -0,0 +1,7 @@
+wget 'https://github.com/houzi-/CDN/raw/master/binary/v3.8.5/arm' -O bin/arm
+wget 'https://github.com/houzi-/CDN/raw/master/binary/v3.8.5/x86' -O bin/x86
+wget 'https://github.com/houzi-/CDN/raw/master/binary/v3.8.5/mips' -O bin/mips
+wget 'https://github.com/houzi-/CDN/raw/master/binary/v3.8.5/mipsel' -O bin/mipsel
+wget 'https://github.com/houzi-/CDN/raw/master/binary/v3.8.5/aarch64' -O bin/aarch64
+wget 'https://github.com/houzi-/CDN/raw/master/binary/v3.8.5/x86_64' -O bin/x86_64
+chmod +x bin/*
